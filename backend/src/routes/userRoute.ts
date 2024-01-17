c519487eeac525d7960c6ac2294d7eb9d709c36f
Aammya Sapra <aasapra@ucsd.edu>
2024-01-17 08:29:29 -0800

    Initialize Firebase Admin SDK

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,20 +1,28 @@
-// userRoute.ts
 import express, { Request, Response } from 'express';
-import User from '../models/User'; // Adjust the path based on your project structure
+import User from '../models/User'; 
 import admin from 'firebase-admin';
+import path from 'path';
 
 const router = express.Router();
+
+// Initialize Firebase Admin SDK 
+const serviceAccountPath = path.resolve(__dirname, '../../firebase/fb_service_account_key.json'); 
+const serviceAccount = require(serviceAccountPath);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
 
 router.use(express.json());
 
 router.post('/api/createUser', async (req: Request, res: Response) => {
   try {
-    const { name, gender, accountType, approvalStatus } = req.body;
+    const { name, gender, accountType, approvalStatus, email, password } = req.body;
 
     // Create user in Firebase Authentication
     const userRecord = await admin.auth().createUser({
-      email: req.body.email,
-      password: req.body.password,
+      email,
+      password,
     });
 
     // Create user in MongoDB with additional details
