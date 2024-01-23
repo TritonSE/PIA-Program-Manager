288f5b95f5dc0441103d9b0e216e89c27e2c9dcd
Victor Siu <siuleungjing@gmail.com>
2024-01-23 15:32:57 -0800

    Add login page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,55 +1,113 @@
+import Image from "next/image";
+import { useMemo } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
-import { Checkbox } from "../components/Checkbox";
-import Radio from "../components/Radio";
-import { Textfield } from "../components/Textfield";
+import { Textfield } from "@/components/Textfield";
+import { useWindowSize } from "@/hooks/useWindowSize";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
-  const dietaryList = ["Nuts", "Eggs", "Seafood", "Pollen", "Dairy", "Other"];
-
   const { register, setValue, handleSubmit } = useForm();
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
-
+  const { width } = useWindowSize();
+  const isMobile = useMemo(() => width <= 640, [width]);
+  const isTablet = useMemo(() => width <= 1300, [width]);
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col items-center justify-between gap-10 p-12"
-    >
-      <div className="grid gap-5">
-        <Textfield register={register} name={"firstname"} label="First" placeholder="John" />
-        <Textfield
-          register={register}
-          name={"email"}
-          label={"Email"}
-          type="email"
-          placeholder="johnsmith@gmail.com"
-        />
-        <Textfield
-          register={register}
-          setValue={setValue}
-          name={"date"}
-          label="Date"
-          placeholder="00/00/0000"
-          calendar={true}
-        />
-      </div>
+    <main className="flex h-screen w-full items-center justify-center">
+      <div className="flex h-full w-full items-center justify-center">
+        <div
+          className={cn(
+            "flex h-full flex-col",
+            isMobile ? "mt-[20%] w-[80%]" : "mb-[8%] w-[65%] justify-center",
+          )}
+        >
+          {isMobile && (
+            <div className="flex flex-col justify-center">
+              <div className="flex h-full flex-col justify-center">
+                <div className="flex flex-col items-center">
+                  <Image
+                    alt="company logo"
+                    src="/sidebar/logo.png"
+                    width={130}
+                    height={130}
+                    className=""
+                  />
+                </div>
+                <div className="mb-5 mt-5 flex flex-col items-center">
+                  <h1 className="font-[alternate-gothic] text-3xl max-lg:text-3xl">WELCOME TO</h1>
+                  <h1 className="font-[alternate-gothic] text-3xl max-lg:text-3xl">
+                    PLANT IT AGAIN!
+                  </h1>
+                </div>
+              </div>
+            </div>
+          )}
+          {!isMobile && (
+            <div>
+              <h1 className="font-[alternate-gothic] text-5xl text-black max-lg:text-5xl">
+                Sign in to PIA
+              </h1>
+              <h1 className="text-1xl max-lg:text-1xl mb-6 text-black text-pia_accent">
+                Don&lsquo;st have an account?{" "}
+                <a className="text-blue text-1xl max-lg:text-1xl text-pia_accent">Sign up</a>
+              </h1>
+            </div>
+          )}
 
-      <div className="grid w-full sm:w-1/2 ">
-        <h2 className="mb-2 text-pia_accent">Dietary Restrictions</h2>
-        <Checkbox register={register} name="dietary" options={dietaryList} />
+          <div className="grid gap-5">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex w-full flex-col justify-between gap-8"
+            >
+              <div>
+                <h1 className="text-lg font-light text-black text-pia_accent max-lg:text-lg">
+                  Email Address
+                </h1>
+                <Textfield
+                  register={register}
+                  name={"email"}
+                  label={""}
+                  type="email"
+                  placeholder="name@email.com"
+                />
+              </div>
+              <div>
+                <h1 className="text-lg font-light text-black text-pia_accent max-lg:text-lg">
+                  Password
+                </h1>
+                <Textfield
+                  register={register}
+                  setValue={setValue}
+                  name={"date"}
+                  label=""
+                  placeholder="Enter Password"
+                />
+                <h1
+                  className={cn(
+                    "mt-1 text-black text-pia_accent",
+                    isTablet ? "" : "text-right",
+                    isMobile ? "text-sm max-lg:text-sm" : "text-lg max-lg:text-lg",
+                  )}
+                >
+                  Forgot Password?
+                </h1>
+              </div>
+              <button type="submit" className="rounded-md bg-pia_dark_green px-5 py-3 text-white">
+                Sign In
+              </button>
+              {isMobile && (
+                <h1 className={cn("text-sm text-black text-pia_accent max-lg:text-sm")}>
+                  Don&lsquo;t have an account?{" "}
+                  <a className="text-blue text-sm text-pia_accent max-lg:text-sm">Sign up</a>
+                </h1>
+              )}
+            </form>
+          </div>
+        </div>
       </div>
-
-      <div className="">
-        <h2 className="mb-5 text-2xl font-bold">Gender</h2>
-        <Radio register={register} name="gender" options={["Male", "Female", "Rather not say"]} />
-      </div>
-
-      <button type="submit" className="rounded-md bg-pia_dark_green px-5 py-3 text-white">
-        Submit
-      </button>
-    </form>
+    </main>
   );
 }
