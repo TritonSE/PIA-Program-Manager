c4ddfee4c44d8629f2840f7339fc7af36bbcbdae
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2024-01-23 09:54:40 -0800

    Added Button Components (#33)
    * Added Button Components
    
    * fixed lint error for Button components

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app";
 
 import Navigation from "../components/Navigation";
 import "../styles/global.css";
+import "../styles/globals.css";
 
 function App({ Component, pageProps }: AppProps) {
   return (
