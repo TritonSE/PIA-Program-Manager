83e14ffacf288dda9f05c65dac2a7d3fc14e9e3d
Jacob Lee <JLpython@outlook.com>
2024-01-30 23:30:30 -0800

    feat: create parseProgram() function to parse program JSON data

--- frontend/src/api/programs.ts
+++ frontend/src/api/programs.ts
@@ -29,3 +29,21 @@ export interface ProgramJSON {
     end: string;
     color: "teal" | "yellow" | "red" | "green";
 }
+
+
+/**
+ * 
+ * @param program 
+ * @returns 
+ */
+function parseProgram(program: ProgramJSON): Program {
+    return {
+        _id: program._id,
+        name: program.name,
+        abbr: program.abbr,
+        type: program.type,
+        start: new Date(program.type),
+        end: new Date(program.type),
+        color: program.color
+    };
+}
