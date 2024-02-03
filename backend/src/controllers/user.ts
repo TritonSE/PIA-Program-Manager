b6ee7cbc73f90649b66177e095803d77961f80ff
adhi0331 <adhi03.ananthan@gmail.com>
2024-02-03 10:29:24 -0800

    fix lint errors

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,11 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import admin from "firebase-admin";
 
-import { ValidationError } from "../errors";
-import { errorHandler } from "../errors/handler";
 import User, { UserDocument } from "../models/user";
 import { firebaseAuth } from "../util/firebase";
+import validationErrorParser from "../util/validationErrorParser";
 
 // Define the type for req.body
 type CreateUserRequestBody = {
@@ -18,21 +17,13 @@ type CreateUserRequestBody = {
 export const createUser = async (
   req: Request<Record<string, never>, Record<string, never>, CreateUserRequestBody>,
   res: Response,
+  nxt: NextFunction,
 ) => {
   try {
     // Check for validation errors
     const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //   return res.status(400).json({ errors: errors.array() });
-    // }
-    if (!errors.isEmpty()) {
-      let errStr = "";
-      for (const err of errors.array()) {
-        errStr += err.msg + " ";
-      }
-      errStr = errStr.trim();
-      throw new ValidationError(0, 400, errStr);
-    }
+
+    validationErrorParser(errors);
 
     const { name, accountType, email, password } = req.body;
 
@@ -45,28 +36,17 @@ export const createUser = async (
     // Set custom claim for accountType (“admin” | “team”)
     await firebaseAuth.setCustomUserClaims(userRecord.uid, { accountType });
 
-    // Create user in MongoDB
-    const newUser: UserDocument = new User({
+    const newUser2 = await User.create({
       _id: userRecord.uid, // Set document id to firebaseUID (Linkage between Firebase and MongoDB)
       name,
       accountType,
       // approvalStatus default false in User constructor
-    } as UserDocument); // Type assertion
+    } as UserDocument);
 
-    await newUser.save();
-
-    // const newUser = await User.create({
-    //   _id: userRecord.uid, // Set document id to firebaseUID (Linkage between Firebase and MongoDB)
-    //   name,
-    //   accountType,
-    //   // approvalStatus default false in User constructor
-    // } as UserDocument);
-
-    res.status(201).json({ message: "User created successfully" });
-    // res.status(201).json(newUser);
+    res.status(201).json(newUser2);
   } catch (error) {
     console.error(error);
-    errorHandler(ValidationError.USER_CREATION_UNSUCCESSFUL, req, res);
+    nxt(error);
   }
 
   return;
--- backend/src/errors/validation.ts
+++ backend/src/errors/validation.ts
@@ -3,5 +3,8 @@ import { CustomError } from "./errors";
 const USER_CREATION_UNSUCCESSFUL = "User not created successfully";
 
 export class ValidationError extends CustomError {
+  constructor(code: number, status: number, message: string) {
+    super(code, status, "VALIDATION ERROR: " + message);
+  }
   static USER_CREATION_UNSUCCESSFUL = new ValidationError(1, 400, USER_CREATION_UNSUCCESSFUL);
 }
