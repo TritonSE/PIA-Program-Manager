96bd8f0fdcbfd6f23ff545435bb65893634a6ff5
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2024-02-10 14:20:19 -0800

    fix: use generic type for input components

--- frontend/src/components/StudentForm/StudentBackground.tsx
+++ frontend/src/components/StudentForm/StudentBackground.tsx
@@ -4,13 +4,13 @@ import { cn } from "../../lib/utils";
 import { Checkbox } from "../Checkbox";
 import { Textfield } from "../Textfield";
 
-import { FormData, StudentFormData } from "./types";
+import { StudentData, StudentFormData } from "./types";
 
 type StudentBackgroundProps = {
-  register: UseFormRegister<FormData>;
+  register: UseFormRegister<StudentFormData>;
   classname?: string;
-  setCalendarValue: UseFormSetValue<FormData>;
-  data: StudentFormData | null;
+  setCalendarValue: UseFormSetValue<StudentFormData>;
+  data: StudentData | null;
 };
 
 const dietaryList = ["Nuts", "Eggs", "Seafood", "Pollen", "Dairy", "Other"];
