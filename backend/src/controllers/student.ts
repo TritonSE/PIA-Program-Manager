91a6fc01a3fce224528bfe058b1322a8e73b7ab2
parth4apple <72187062+parth4apple@users.noreply.github.com>
2024-02-21 02:24:31 -0800

    feat: add edit student route code

--- backend/src/controllers/student.ts
+++ backend/src/controllers/student.ts
@@ -25,10 +25,33 @@ export type typedModel = {
   birthday: string;
   intakeDate: string;
   tourDate: string;
-  prog1: string[];
-  prog2: string[];
+  regularPrograms: string[];
+  varyingPrograms: string[];
   dietary: string[];
   otherString: string;
+};
+
+type Contact = {
+  lastName: string;
+  firstName: string;
+  email: string;
+  phoneNumber: string;
+};
+
+type StudentJSON = {
+  _id: string;
+  student: Contact;
+  emergency: Contact;
+  serviceCoordinator: Contact;
+  location: string;
+  medication?: string;
+  birthday: Date;
+  intakeDate: Date;
+  tourDate: Date;
+  regularPrograms: string[];
+  varyingPrograms: string[];
+  dietary: string[];
+  otherString?: string;
 };
 
 export const createStudent: RequestHandler = async (req, res, next) => {
@@ -40,6 +63,43 @@ export const createStudent: RequestHandler = async (req, res, next) => {
     const newStudent = await StudentModel.create(req.body as typedModel);
 
     res.status(201).json(newStudent);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editStudent: RequestHandler = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    validationErrorParser(errors);
+
+    const studentId = req.params.id;
+    const studentData = req.body as StudentJSON;
+
+    if (studentId !== studentData._id) {
+      return res.status(400).json({ message: "Invalid student ID" });
+    }
+
+    const editedStudent = await StudentModel.findOneAndUpdate({ _id: studentId }, studentData, {
+      new: true,
+    });
+
+    if (!editedStudent) {
+      return res.status(404).json({ message: "No object in database with provided ID" });
+    }
+
+    res.status(200).json(editedStudent);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllStudents: RequestHandler = async (_, res, next) => {
+  try {
+    const students = await StudentModel.find();
+
+    res.status(200).json(students);
   } catch (error) {
     next(error);
   }
