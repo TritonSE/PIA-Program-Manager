50ecc84e648f215ed8174ba7ed889282a15e64c7
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2024-02-25 18:43:13 -0800

    Finished editing Programs Page

--- frontend/src/components/ProgramCard.tsx
+++ frontend/src/components/ProgramCard.tsx
@@ -1,8 +1,9 @@
 import { Poppins } from "next/font/google";
 import Image from "next/image";
-import React from "react";
+import React, { useMemo } from "react";
 
-import styles from "../styles/ProgramCard.module.css";
+import { useWindowSize } from "../hooks/useWindowSize";
+import { cn } from "../lib/utils";
 
 const poppins = Poppins({ weight: ["400", "700"], style: "normal", subsets: [] });
 
@@ -16,48 +17,61 @@ export type CardProps = {
 };
 
 export function ProgramCard({ type, title, dates, numStudents, color, className }: CardProps) {
-  let outerDivClass = styles.outerDiv;
+  const { width } = useWindowSize();
+  const isTablet = useMemo(() => width < 1024, [width]);
 
-  let topDivClass = styles.topDiv;
+  let outerDivClass = "text-white grow overflow-hidden tracking-wide leading-6";
+  let topDivClass = "";
+  let botDivClass = "text-black bg-white";
+  let typeClass;
+  let titleClass;
+  let dateClass;
+  let numClass;
+  let numTextClass;
+  let iconClass = "relative";
+
+  if (isTablet) {
+    outerDivClass += " rounded-lg h-36";
+    topDivClass += " h-20";
+    botDivClass += " h-16";
+    typeClass = cn("uppercase relative text-[10px] top-2 left-3", poppins.className);
+    titleClass = cn("capitalize relative text-sm top-2 left-3 font-bold", poppins.className);
+    dateClass = cn("relative text-[10px] top-2 left-3", poppins.className);
+    numClass = "h-5 gap-x-1.5 flex flex-row relative top-2 left-3";
+    numTextClass = cn("text-[10px]", poppins.className);
+    iconClass = "h-2 w-3 mt-[7px]";
+  } else {
+    outerDivClass += " rounded-2xl h-68";
+    topDivClass += " h-36";
+    botDivClass += " h-32";
+    typeClass = cn("uppercase relative text-sm top-5 left-7", poppins.className);
+    titleClass = cn("capitalize relative text-3xl top-8 left-7 font-bold", poppins.className);
+    dateClass = cn("relative text-base top-5 left-7", poppins.className);
+    numClass = "h-8 gap-x-1.5 flex flex-row relative top-14 left-7";
+    numTextClass = cn("text-base", poppins.className);
+    iconClass = "h-3 w-[18px] mt-[5px]";
+  }
+
   switch (color) {
     case 1:
-      topDivClass += ` ${styles.teal}`;
+      topDivClass += " bg-secondary_teal";
       break;
     case 2:
-      topDivClass += ` ${styles.yellow}`;
+      topDivClass += " bg-secondary_yellow";
       break;
     case 3:
-      topDivClass += ` ${styles.red}`;
+      topDivClass += " bg-secondary_red";
       break;
     case 4:
-      topDivClass += ` ${styles.green}`;
+      topDivClass += " bg-secondary_green";
       break;
     default:
-      topDivClass += ` ${styles.teal}`;
+      topDivClass += " bg-secondary_teal";
       break;
   }
 
-  const botDivClass = styles.botDiv;
-
-  let typeClass = styles.type;
-  typeClass += ` ${poppins.className}`;
-
-  let titleClass = styles.title;
-  titleClass += ` ${poppins.className}`;
-
-  let dateClass = styles.dates;
-  dateClass += ` ${poppins.className}`;
-
-  let numClass = styles.numStudents;
-  numClass += ` ${poppins.className}`;
-
-  let numTextClass = styles.numText;
-  numTextClass += ` ${poppins.className}`;
-
-  const iconClass = styles.icon;
-
   if (className) {
-    outerDivClass += ` ${className}`;
+    outerDivClass = cn(outerDivClass, className);
   }
 
   return (
--- frontend/src/pages/programs.tsx
+++ frontend/src/pages/programs.tsx
@@ -1,70 +1,97 @@
-import { Button } from "../components/Button";
+import React, { useMemo } from "react";
+
 import { ProgramCard } from "../components/ProgramCard";
-import styles from "../styles/Programs.module.css";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 export default function Programs() {
-  const cardsClass = styles.cards;
-  const mainClass = styles.main;
-  const headerClass = styles.header;
-  let titleClass = styles.title;
-  titleClass += ` font-[alternate-gothic]`;
-  const spacerClass = styles.spacer;
-  const grouperClass = styles.group;
-  const addTaskClass = styles.addTask;
+  const { width } = useWindowSize();
+  const isMobile = useMemo(() => width < 640, [width]);
+  const isTablet = useMemo(() => width < 1024, [width]);
+  const extraLarge = useMemo(() => width >= 2000, [width]);
+
+  let mainClass = "h-full overflow-y-scroll no-scrollbar";
+  let titleClass = "font-[alternate-gothic]";
+  let headerClass = "flex flex-row";
+  let addTaskClass = "m-0 rounded-3xl bg-pia_dark_green text-white";
+  let cardsGridClass = "grid";
+  let cardClass = "";
+
+  if (isTablet) {
+    titleClass += " text-2xl leading-none h-6";
+    mainClass += " p-0";
+    addTaskClass += " text-[10px] h-6 px-[10px]";
+
+    if (isMobile) {
+      headerClass += " pt-2 pb-3";
+      cardsGridClass += " grid-cols-1";
+      cardClass += " p-0 py-3";
+    } else {
+      cardsGridClass += " grid-cols-2";
+      headerClass += " p-2 py-4";
+      cardClass += " p-2";
+    }
+  } else {
+    titleClass += " text-[40px] leading-none h-10";
+    headerClass += " p-5 pt-10 pb-5";
+    addTaskClass += " text-base h-12 px-6";
+    cardClass += " p-5";
+
+    if (extraLarge) {
+      cardsGridClass += " grid-cols-3 max-w-[1740px]";
+      headerClass += " max-w-[1740px]";
+    } else {
+      cardsGridClass += " grid-cols-2 max-w-[1160px]";
+      headerClass += " max-w-[1160px]";
+    }
+  }
 
   return (
     <main className={mainClass}>
       <div className={headerClass}>
         <h1 className={titleClass}>Programs</h1>
-        <div className={spacerClass}></div>
-        <div className={grouperClass}>
-          {/* Should be replaced with Add Button when created */}
-          <Button label="+ Create Program" className={addTaskClass}></Button>
-        </div>
+        <div className="grow"></div>
+        {/* Should be replaced with Add Button when created */}
+        <button type="submit" className={addTaskClass}>
+          + Create Program
+        </button>
       </div>
-      <div className={cardsClass}>
-        <ProgramCard
-          type="Standard"
-          title="Test"
-          dates="Jun 12, 2023 - Jun 12, 2024"
-          numStudents={1}
-          color={1}
-        />
-        <ProgramCard
-          type="Standard"
-          title="Test"
-          dates="Jun 12, 2023 - Jun 12, 2024"
-          numStudents={1}
-          color={2}
-        />
-        <ProgramCard
-          type="Standard"
-          title="Test"
-          dates="Jun 12, 2023 - Jun 12, 2024"
-          numStudents={1}
-          color={3}
-        />
-        <ProgramCard
-          type="Standard"
-          title="Test"
-          dates="Jun 12, 2023 - Jun 12, 2024"
-          numStudents={1}
-          color={3}
-        />
-        <ProgramCard
-          type="Standard"
-          title="Test"
-          dates="Jun 12, 2023 - Jun 12, 2024"
-          numStudents={1}
-          color={3}
-        />
-        <ProgramCard
-          type="Standard"
-          title="Test"
-          dates="Jun 12, 2023 - Jun 12, 2024"
-          numStudents={1}
-          color={3}
-        />
+      <div className={cardsGridClass}>
+        <div className={cardClass}>
+          <ProgramCard
+            type="Standard"
+            title="Intro"
+            dates="Jun 12, 2023 - Jun 12, 2024"
+            numStudents={1}
+            color={1}
+          />
+        </div>
+        <div className={cardClass}>
+          <ProgramCard
+            type="Standard"
+            title="Intro"
+            dates="Jun 12, 2023 - Jun 12, 2024"
+            numStudents={1}
+            color={1}
+          />
+        </div>
+        <div className={cardClass}>
+          <ProgramCard
+            type="Standard"
+            title="Intro"
+            dates="Jun 12, 2023 - Jun 12, 2024"
+            numStudents={1}
+            color={1}
+          />
+        </div>
+        <div className={cardClass}>
+          <ProgramCard
+            type="Standard"
+            title="Intro"
+            dates="Jun 12, 2023 - Jun 12, 2024"
+            numStudents={1}
+            color={1}
+          />
+        </div>
       </div>
     </main>
   );
