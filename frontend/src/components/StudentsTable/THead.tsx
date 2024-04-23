59fc3ef57411813fc4f108499d5399826d299edb
parth4apple <72187062+parth4apple@users.noreply.github.com>
2024-04-22 23:30:16 -0700

    feat: filtering + designer feedback

--- frontend/src/components/StudentsTable/THead.tsx
+++ frontend/src/components/StudentsTable/THead.tsx
@@ -1,41 +1,34 @@
 import { HeaderGroup, Table, flexRender } from "@tanstack/react-table";
-import { useContext } from "react";
 
 import DebouncedInput from "../DebouncedInput";
-import StudentFormButton from "../StudentFormButton";
 import { TableHead, TableHeader, TableRow } from "../ui/table";
 
-import ProgramFilter from "./ProgramFilter";
-import { StudentMap, StudentTableRow } from "./types";
-
-import { UserContext } from "@/contexts/user";
-import { useWindowSize } from "@/hooks/useWindowSize";
+import { ProgramFilter, StatusFilter } from "./FilterFns";
+import { StudentTableRow } from "./types";
 
 function TableActionsHeader({
   headerGroup,
   globalFilter,
   setGlobalFilter,
-  setAllStudents,
 }: {
   headerGroup: HeaderGroup<StudentTableRow>;
   globalFilter: string;
   setGlobalFilter: React.Dispatch<React.SetStateAction<string>>;
-  setAllStudents: React.Dispatch<React.SetStateAction<StudentMap>>;
 }) {
-  const { isTablet } = useWindowSize();
-
-  const { isAdmin } = useContext(UserContext);
-
   return (
     <TableRow key={headerGroup.id + "1"} className="border-b">
       <TableHead className="h-6 w-full px-10 py-5" colSpan={6}>
         <div className="flex justify-between">
           <span className="flex gap-6">
-            {headerGroup.headers.map((header) =>
-              header.column.getCanFilter() ? (
-                <ProgramFilter key={header.id} column={header.column} />
-              ) : null,
-            )}
+            {headerGroup.headers.map((header) => {
+              if (!header.column.getCanFilter()) return null;
+              if (["Curr. Program 1", "Curr. P1"].includes(header.column.id)) {
+                return <ProgramFilter key={header.id} column={header.column} />;
+              } else if (["Curr. Program 2", "Curr. P2"].includes(header.column.id)) {
+                return <StatusFilter key={header.id} column={header.column} />;
+              }
+              return null;
+            })}
             <div className="w-[200px]">
               <DebouncedInput
                 value={globalFilter ?? ""}
@@ -47,7 +40,6 @@ function TableActionsHeader({
               />
             </div>
           </span>
-          {!isTablet && isAdmin && <StudentFormButton type="add" setAllStudents={setAllStudents} />}
         </div>
       </TableHead>
     </TableRow>
@@ -58,7 +50,10 @@ function TableDataHeader({ headerGroup }: { headerGroup: HeaderGroup<StudentTabl
   return (
     <TableRow key={headerGroup.id + "2"} className="border-b">
       {headerGroup.headers.map((header) => (
-        <TableHead key={header.id} className="h-6 px-0 py-7 first:pl-10 last:pr-10 last:text-right">
+        <TableHead
+          key={header.id}
+          className="h-6 px-0 py-7 text-neutral-800 first:pl-10 last:pr-10 last:text-right"
+        >
           {header.isPlaceholder
             ? null
             : flexRender(header.column.columnDef.header, header.getContext())}
@@ -71,12 +66,10 @@ function TableDataHeader({ headerGroup }: { headerGroup: HeaderGroup<StudentTabl
 export default function THead({
   globalFilter,
   setGlobalFilter,
-  setAllStudents,
   table,
 }: {
   globalFilter: string;
   setGlobalFilter: React.Dispatch<React.SetStateAction<string>>;
-  setAllStudents: React.Dispatch<React.SetStateAction<StudentMap>>;
   table: Table<StudentTableRow>;
 }) {
   return (
@@ -87,7 +80,6 @@ export default function THead({
             headerGroup={headerGroup}
             globalFilter={globalFilter}
             setGlobalFilter={setGlobalFilter}
-            setAllStudents={setAllStudents}
           />
           <TableDataHeader headerGroup={headerGroup} />
         </>
